7315bf5004c3d3975780ac5feac30ff470934102
Alyona Vasjucenko <alyona.vasjucenko@gmail.com>
2023-11-30 19:58:01 +0200

    [master]: Add project

---
+++ src/main/java/lv/acodemy/utils/LocalDriverManager.java
@@ -0,0 +1,55 @@
+package lv.acodemy.utils;
+
+import lombok.SneakyThrows;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LocalDriverManager {
+
+   private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    private LocalDriverManager() {
+    }
+
+    public static WebDriver getInstance() {
+        if(driver.get() == null) {
+            if(ConfigurationProperties.getConfiguration().getBoolean("run.locally")) {
+                driver.set(new ChromeDriver());
+            } else {
+                driver.set(configureRemote());
+            }
+        } else {
+           return driver.get();
+        }
+        return driver.get();
+    }
+
+    public static void closeDriver() {
+        driver.get().close();
+        driver.get().quit();
+        driver.remove();
+    }
+
+    @SneakyThrows
+    public static RemoteWebDriver configureRemote() {
+        ChromeOptions browserOptions = new ChromeOptions();
+        browserOptions.setPlatformName("Windows 10");
+        browserOptions.setBrowserVersion("latest-1");
+        Map<String, Object> sauceOptions = new HashMap<>();
+        sauceOptions.put("username", "oauth-alyona.vasjucenko-667e8");
+        sauceOptions.put("accessKey", "bb4f9c89-1376-4920-86cf-fc12b0f578f2");
+        sauceOptions.put("build", "selenium-build-63P9R");
+        sauceOptions.put("name", "<your test name>");
+        browserOptions.setCapability("sauce:options", sauceOptions);
+
+        URL url = new URL("https://ondemand.eu-central-1.saucelabs.com:443/wd/hub");
+        return new RemoteWebDriver(url, browserOptions);
+    }
+}
---
+++ src/test/java/SauceDemoTest.java
@@ -0,0 +1,68 @@
+import lv.acodemy.page_object.sauce_pages.InventoryPage;
+import lv.acodemy.page_object.sauce_pages.LoginPage;
+import lv.acodemy.utils.LocalDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+import static lv.acodemy.utils.ConfigurationProperties.*;
+import static lv.acodemy.utils.LocalDriverManager.*;
+import static lv.acodemy.utils.constants.ErrorMessages.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.openqa.selenium.support.ui.ExpectedConditions.*;
+
+public class SauceDemoTest {
+
+    WebDriver driver;
+    LoginPage loginPage;
+    InventoryPage inventoryPage;
+    Wait<WebDriver> wait;
+
+    @BeforeMethod
+    public void before() {
+        driver = LocalDriverManager.getInstance();
+        loginPage = new LoginPage();
+        inventoryPage = new InventoryPage();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+    }
+
+    @Test(description = "Test successful login")
+    public void testLogin() {
+        driver.get(getConfiguration().getString("sauce.demo.url"));
+        loginPage.authorize(getConfiguration().getString("username"), getConfiguration().getString("password"));
+        inventoryPage.getInventoryItems();
+        wait.until(visibilityOfAllElements(inventoryPage.getInventoryItems()));
+        assertThat(inventoryPage.getInventoryItems()).hasSize(6);
+    }
+
+    @Test(description = "Test authorization incorrect password")
+    public void testLoginIncorrectCredentials() {
+        driver.get(getConfiguration().getString("sauce.demo.url"));
+        loginPage.authorize("standard_user", "incorrect_password");
+        assertThat(loginPage.getErrorMessage().getText()).isEqualTo(USER_AND_PASSWORD_DO_NOT_MATCH);
+    }
+
+    @Test(description = "Test authorization no login and password")
+    public void testLoginNoCredentials() {
+        driver.get(getConfiguration().getString("sauce.demo.url"));
+        loginPage.authorize("", "");
+        assertThat(loginPage.getErrorMessage().getText()).isEqualTo(USER_NAME_IS_REQUIRED);
+    }
+
+    @Test(description = "Test authorization login with empty password")
+    public void testLoginNoPassword() {
+        driver.get(getConfiguration().getString("sauce.demo.url"));
+        loginPage.authorize("standard_user", "");
+        assertThat(loginPage.getErrorMessage().getText()).isEqualTo(PASSWORD_IS_REQUIRED);
+    }
+
+    @AfterMethod
+    public void after() {
+        closeDriver();
+    }
+}
